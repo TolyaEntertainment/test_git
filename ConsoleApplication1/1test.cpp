e1edb45c495f2dc221840afa266ba8350c68aaeb
Анатолий <tolik.d03@gmail.com>
2025-02-16 19:39:07 +0300

    Изучили переменные, типы данных, числа с точкой, хранение символа , булевую или логический тип данных

--- ConsoleApplication1/1test.cpp
+++ ConsoleApplication1/1test.cpp
@@ -4,8 +4,29 @@ int main() {
 	
 	setlocale(LC_ALL, "RU");
 
-	std::cout << "Berserk\n\n";
-	std::cout << "Берсерк\n";
-	//gg
+	int num1, num2; // от -2милиард до 2милиард, 4 байт
+	std::cout << " введите переменную num1: ";
+	std::cin >> num1;
+
+	std::cout << " введите переменную num2: ";
+	std::cin >> num2;
+
+	std::cout << "num1: " << num1 << ". num2: " << num2;
+	
+	// Типы данных
+	short num3 = 3; // от -32к до 32к, 2 байт
+	unsigned short num4 = 4; // от 0к до 65к, 2 байт
+	long num5 = 5; // 8 байт
+
+	// числа с точкой
+	float num6 = 6.6f;
+	double num7 = 7.7777f;
+
+	// Хранение символа
+	char sym1 = 't';
+	// Логический тип данных
+	bool siPonyal = true;
+
+
 	return 0;
 }
