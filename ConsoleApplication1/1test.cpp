0012d4f9192652bd9ca85ba402674d3c94a8ae56
Анатолий <tolik.d03@gmail.com>
2025-02-19 18:54:15 +0300

    Операторы

--- ConsoleApplication1/1test.cpp
+++ ConsoleApplication1/1test.cpp
@@ -1,32 +1,25 @@
-#include <iostream>
+#include <iostream> 
 
 int main() {
-	
+	system("chcp 1251");
 	setlocale(LC_ALL, "RU");
 
-	int num1, num2; // от -2милиард до 2милиард, 4 байт
-	std::cout << " введите переменную num1: ";
+	int num1;
+	std::cout << "введите число: ";
 	std::cin >> num1;
+	if (num1 > 3) {
+		std::cout << "num1 > 3\n";
+	}
 
-	std::cout << " введите переменную num2: ";
-	std::cin >> num2;
+	else if (num1 == 3) {
+		std::cout << "num1 = 3\n";
 
-	std::cout << "num1: " << num1 << ". num2: " << num2;
+	}
+	else {
+		std::cout << "num1 <3\n";
+
+	}
 	
-	// Типы данных
-	short num3 = 3; // от -32к до 32к, 2 байт
-	unsigned short num4 = 4; // от 0к до 65к, 2 байт
-	long num5 = 5; // 8 байт
-
-	// числа с точкой
-	float num6 = 6.6f;
-	double num7 = 7.7777f;
-
-	// Хранение символа
-	char sym1 = 't';
-	// Логический тип данных
-	bool siPonyal = true;
-
-
 	return 0;
+
 }
