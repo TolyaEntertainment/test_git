fc665657acaec335c85076c2b07385b3052ccd6f
Анатолий <tolik.d03@gmail.com>
2025-02-19 19:10:26 +0300

    Операторы

--- ConsoleApplication1/1test.cpp
+++ ConsoleApplication1/1test.cpp
@@ -4,10 +4,11 @@ int main() {
 	system("chcp 1251");
 	setlocale(LC_ALL, "RU");
 
+	bool I_understood = true;
 	int num1;
 	std::cout << "введите число: ";
 	std::cin >> num1;
-	if (num1 > 3) {
+	if (num1 > 3 || I_understood == false) { // !I_understood
 		std::cout << "num1 > 3\n";
 	}
 
